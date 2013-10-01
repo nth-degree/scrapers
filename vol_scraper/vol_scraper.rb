a1bda5b0fa9609db4abb06965b274d0ca3eeb548
Kumar Dandapani <kumar@140proof.com>
2013-09-30 17:46:03 -0700

    updated options scraper

--- vol_scraper/vol_scraper.rb
+++ vol_scraper/vol_scraper.rb
@@ -3,14 +3,16 @@ require 'restclient'
 require 'date'
 
 def get_vol_data(tckr)
+tckr=tckr.gsub(/\n/,"")
+alllines=""
 url = 'http://finance.yahoo.com/q/os?s='
 url2 = "&m=2013-11"
+
+begin
 page = Nokogiri::HTML(RestClient.get(url+tckr+url2))
-alllines=""
 
 ytckr = tckr.downcase
 underlying = page.xpath('//*[@id="yfs_l84_'+ ytckr +'"]').text
-puts underlying
 num_options = page.xpath('//*[@id="content"]/table/tr[4]/td[1]/table[5]/tr[1]/td[1]/table/tr[1]/td[1]/table').children.count
 
 	for i in 2..num_options
@@ -21,7 +23,9 @@ num_options = page.xpath('//*[@id="content"]/table/tr[4]/td[1]/table[5]/tr[1]/td
 		end
 		alllines << thisline << "\n"
 	end
-	puts alllines 
+rescue
+end
+return alllines
 end
 
 def run_all(infile, outfile)
